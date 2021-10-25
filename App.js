a684384ba64fd457b75794434971f2fd6292c356
Edward Sitar <edwardsitar4@gmail.com>
2021-10-24 20:19:42 -0400

    more merge

--- App.js
+++ App.js
@@ -75,7 +75,7 @@ const App: () => Node = () => {
       style={styles.img} />
           <Section title="Step One">
             Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits. Testing changes.
+            screen and then come back to see your edits. Testing changes. More changes.
           </Section>
           <Section title="See Your Changes">
             <ReloadInstructions />
