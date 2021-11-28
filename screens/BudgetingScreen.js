dd060d3805d685ab0e5b3971d4780a3d30b88731
joshy <joshjarriba@gmail.com>
2021-11-28 16:52:59 -0500

    added a 'enter budget amount' finction

--- screens/BudgetingScreen.js
+++ screens/BudgetingScreen.js
@@ -4,13 +4,23 @@ import React from 'react';
 class BudgetingScreen extends React.Component {
     render() {
       return(
-        <View style={{flex: 1, alignItems:'center', justifyContent:'center'}}>
-            <Button title="Back"
-            onPress={() => this.props.navigation.navigate('Finances')}
-            />
+        <View style={{bottom: 275, flex: 1, alignItems:'center', justifyContent:'center'}}>
+        <TextInput
+        placeholder ="Enter Budget Amount"
+        placeholderTextColor = 'black'
+         />
         </View>
         );
     }
+    style = StyleSheet.create({
+        input: {
+            borderWidth: 1,
+            borderColor: '#777',
+            padding: 8,
+            margin: 10,
+            width: 200,
+        }
+    })
     }
 
 export default BudgetingScreen
