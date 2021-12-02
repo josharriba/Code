e26939b0454c53aa68f4e3680ae183de4e060374
joshy <joshjarriba@gmail.com>
2021-12-01 21:18:13 -0500

    fixed budget interaction kind of

--- screens/BudgetingScreen.js
+++ screens/BudgetingScreen.js
@@ -2,23 +2,36 @@ import React from 'react';
   import { StyleSheet, Button, Text, View, TouchableHighlight, TextInput, StackNavigator } from 'react-native';
 
 class BudgetingScreen extends React.Component {
+  constructor(){
+    super();
+        this.state = {
+            amount: ''
+        }  
+  }
+
+  updateInput = (val, prop) => {
+    const state = this.state;
+    state[prop] = val;
+    this.setState(state);
+  }
     render() {
       return(
         <View style={{bottom: 275, flex: 1, alignItems:'center', justifyContent:'center'}}>
         <TextInput
-        placeholder ="Enter Budget Amount"
+        placeholder ="Enter Budget Amount" 
         placeholderTextColor = 'black'
+        value = {this.state.amount}
+        onChangeText={(input) => this.updateInput(input, 'amount')}
          />
         </View>
         );
     }
-    style = StyleSheet.create({
+    styles = StyleSheet.create({
         input: {
-            borderWidth: 1,
-            borderColor: '#777',
-            padding: 8,
-            margin: 10,
-            width: 200,
+            padding: 15,
+            backgroundColor: '#f8f8f8',
+            borderBottomWidth: 1,
+            borderColor: '#eee',
         }
     })
     }
