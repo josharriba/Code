812cbdd50cf727593a28a42d2c6ee9b47b4a08b3
Daneatrian <drr2@email.sc.edu>
2021-12-03 19:27:19 -0500

    Fixed minor error with news button on the homescreen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -11,13 +11,13 @@ import {
 import firestore from '@react-native-firebase/firestore';
 import db from '../components/FirebaseHandler';
 
-class HomeScreen extends React.Component {  
+class HomeScreen extends React.Component {
   constructor(props) {
     super(props);
-    db.getName(); 
-    this.state= {
-      name: db.name
-    } 
+    db.getName();
+    this.state = {
+      name: db.name,
+    };
   }
 
   signOut = () => {
@@ -25,35 +25,36 @@ class HomeScreen extends React.Component {
     this.props.navigation.navigate('Login');
   };
 
-  
-
-    render() {
-      return(
-        <View style={{padding:5, flex: 1, alignItems:'center'}}> 
-          <Text style={{fontSize: 20, fontWeight: 'bold', padding:50 }}>  
-            Welcome, {this.state.name} !
-          </Text>
-            <Button title="Profile"
-            onPress={() => this.props.navigation.navigate('Profile')}
-            /> 
-            <Button title="Stocks"
-            onPress={() => this.props.navigation.navigate('Stocks')}
-            />
-            <Button title="Dashboard"
-            onPress={() => this.props.navigation.navigate('Dashboard')}
-            />
-            <Button title="Finances"
-            onPress={() => this.props.navigation.navigate('Finances')}
-            />
-            <Button
+  render() {
+    return (
+      <View style={{padding: 5, flex: 1, alignItems: 'center'}}>
+        <Text style={{fontSize: 20, fontWeight: 'bold', padding: 50}}>
+          Welcome, {this.state.name} !
+        </Text>
+        <Button
+          title="Profile"
+          onPress={() => this.props.navigation.navigate('Profile')}
+        />
+        <Button
+          title="Stocks"
+          onPress={() => this.props.navigation.navigate('Stocks')}
+        />
+        <Button
+          title="Dashboard"
+          onPress={() => this.props.navigation.navigate('Dashboard')}
+        />
+        <Button
+          title="Finances"
+          onPress={() => this.props.navigation.navigate('Finances')}
+        />
+        <Button
           title="News"
           onPress={() => this.props.navigation.navigate('News')}
-             <Button title="Sign Out"
-            onPress={() => () => this.signOut()}
-            />
-        </View>
-        );
-    }
+        />
+        <Button title="Sign Out" onPress={() => () => this.signOut()} />
+      </View>
+    );
+  }
 }
 
 export default HomeScreen;
