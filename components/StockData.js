c7d3d3208eea023717a1ac9e7649aa006bf51f87
Edward Sitar <edwardsitar4@gmail.com>
2022-02-20 16:56:36 -0500

    stock screen allows users to seach for stocks

--- components/StockData.js
+++ components/StockData.js
@@ -1,6 +1,7 @@
 import React from 'react';
 //import Plotly from 'react-native-plotly';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, TextInput, Button, Alert} from 'react-native';
+import colors from '../assets/colors/colors';
 // import Plot from 'react-plotly.js';
 
 class StockData extends React.Component {
@@ -8,9 +9,16 @@ class StockData extends React.Component {
     super(props);
     this.state = {
       stockChartXValues: [],
-      stockChartYValues: []
+      stockChartYValues: [], 
+      stockSymbol: ''
     }
   }
+
+  updateInput = (val, prop) => {
+    const state = this.state;
+    state[prop] = val;
+    this.setState(state);
+}
 
   componentDidMount() {
     this.fetchStock();
@@ -21,9 +29,14 @@ class StockData extends React.Component {
     console.log(pointerToThis);
     const API_KEY = 'ER1D6MX3FXC0EQJE';
     // let StockSymbol = 'FB';
-    let API_Call = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=msft&outputsize=compact&apikey=${API_KEY}`;
+    let API_Call = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${this.state.stockSymbol}&outputsize=compact&apikey=${API_KEY}`;
     let stockChartXValuesFunction = [];
     let stockChartYValuesFunction = [];
+
+    //need to have an alert if they enter invalid stock symbol
+    // if(this.state.stockChartXValues == null) {
+    //   Alert.alert("The stock symbol that you entered was invalid. Please try again!")
+    // }
 
     fetch(API_Call)
       .then(function (response) {
@@ -31,6 +44,7 @@ class StockData extends React.Component {
       })
       .then(function (data) {
         console.log(data);
+ 
 
         for (var key in data['Time Series (Daily)']) {
           stockChartXValuesFunction.push(key);
@@ -38,7 +52,6 @@ class StockData extends React.Component {
             data['Time Series (Daily)'][key]['1. open']
           );
         }
-
         console.log(stockChartXValuesFunction);
 
         pointerToThis.setState({
@@ -46,13 +59,44 @@ class StockData extends React.Component {
           stockChartYValues: stockChartYValuesFunction
         });
       });
+    
   }
 
   render() {
-    return (
-      <View>
+    if(this.state.stockSymbol == '') {
+      return (
+        <View style = {styles.container}>
+          <TextInput 
+            style={styles.buttonContainer}
+            placeholder="Search for a stock by entering the stock symbol" 
+            placeholderTextColor={colors.secondary}
+            value = {this.state.stockSymbol} 
+            onChangeText={(input) => this.updateInput(input, 'stockSymbol')}>
+          </TextInput>
+          <Button 
+            style={styles.buttonContainer} 
+            color={colors.primary}
+            title="Search stock" 
+            onPress={() => this.fetchStock()}>
+          </Button>
+        </View>
+      );
+    }
+    else {
+      return (
+        <View style = {styles.container}>
+        <TextInput 
+          placeholder="Search for a stock by entering the stock symbol" 
+          value = {this.state.stockSymbol} 
+          onChangeText={(input) => this.updateInput(input, 'stockSymbol')}>
+        </TextInput>
+        <Button 
+          style={styles.buttonContainer} 
+          title="Search stock" 
+          onPress={() => this.fetchStock()}>
+        </Button>
         <Text style={styles.titleText}> Stock Prices {'\n'}</Text>
-        <Text style={styles.ticker}> MSFT: Microsoft </Text>
+        <Text style={styles.ticker}> Symbol: {this.state.stockSymbol} </Text>
         <Text style={styles.subtitle}> Date: {this.state.stockChartXValues[0]}</Text>
         <Text style={styles.subtitle}> Price: {this.state.stockChartYValues[0]}</Text>
         {/* <Plotly
@@ -68,7 +112,9 @@ class StockData extends React.Component {
           layout={{title: 'MSFT Stock Data', autosize: true}}
         /> */}
       </View>
-    );
+      );
+    }
+    
   }
 }
 
@@ -88,5 +134,58 @@ const styles = StyleSheet.create({
   subtitle: {
     fontFamily: 'sans-serif',
     fontSize: 15,
+  },
+  container: {
+    flex: 1,
+    padding: 10,
+    backgroundColor: "white"
+  },
+  square: {
+    width: 30,
+    height: 12,
+    backgroundColor: "gray"
+  },
+  buttonContainer: {
+    elevation: 8,
+    backgroundColor: colors.background,
+    borderRadius: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 14
+  },
+  buttonText: {
+    padding: 5,
+    marginLeft: '40%',
+    fontFamily: "Montserrat-Medium",
+    fontSize: 20,
+    color: colors.primary
+  },
+  buttonText2: {
+    padding: 5,
+    marginLeft: '30%',
+    fontFamily: "Montserrat-Medium",
+    fontSize: 20,
+    color: colors.primary
+  },
+  text: {
+    fontSize: 15,
+    fontFamily: "Montserrat-Medium",
+    height: 50, width: "100%",
+    borderRadius: 5,
+    paddingHorizontal: 20,
+    borderColor: 'lightgray',
+    borderWidth: 1,
+    marginBottom: 20,
+  },
+  title: {
+    //marginTop: 16,
+    //paddingVertical: 8,
+   // borderWidth: 4,
+    //borderColor: "#20232a",
+    //borderRadius: 6,
+    //backgroundColor: "#61dafb",
+   // color: "#20232a",
+    //textAlign: "center",
+    //fontSize: 30,
+    //fontWeight: "bold"
   }
 });
