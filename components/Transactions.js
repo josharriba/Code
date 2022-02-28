63c250b26964494ccce0b9d56e4ff7c55268dae6
Edward Sitar <edwardsitar4@gmail.com>
2022-02-27 19:37:50 -0500

    display transaction data on dashboard screen. fixes #43. fixes #49

--- components/Transactions.js
+++ components/Transactions.js
@@ -8,44 +8,87 @@ class Transactions extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-        trans: []
+        trans: [], 
+        dates: [], 
+        amounts: [], 
+        descriptions: []
     }
   }
 
-  getTransactions() {
-    //console.log(auth().currentUser.email)
-    firestore().collection('Users').doc(auth().currentUser.email)
-     .collection('Transactions').get()
-     .then(querySnapshot => {
-         querySnapshot.forEach(doc => {
-             const{date, description, amount} = doc.data();
-             this.state.trans.push({
-                 date, 
-                 description, 
-                 amount
-             }); 
-         });
-         //this.state.trans = transactions;
-         // this.setState({
-         //     trans: transactions,
-         // });
-       // console.log(this.state.trans);
-     })
-     .catch((error) => {
-         const errorCode = error.code;
-         const errorMessage = error.message; 
-         alert(errorMessage);
-         throw error;
-     });
-     //console.log(transactions);
- }
+  componentDidMount() {
+    this.getTransactions();
+  }
+
+//   getTransactions() {
+//     //console.log(auth().currentUser.email)
+//     firestore().collection('Users').doc(auth().currentUser.email)
+//      .collection('Transactions').get()
+//      .then(querySnapshot => {
+//          querySnapshot.forEach(doc => {
+//              const{date, description, amount} = doc.data();
+//              this.state.trans.push({
+//                  date, 
+//                  description, 
+//                  amount
+//              }); 
+//          });
+//          //this.state.trans = transactions;
+//          // this.setState({
+//          //     trans: transactions,
+//          // });
+//        //console.log(this.state.trans);
+//        this.mapTransactions();
+//      })
+//      .catch((error) => {
+//          const errorCode = error.code;
+//          const errorMessage = error.message; 
+//          alert(errorMessage);
+//          throw error;
+//      });
+//      //console.log(transactions);
+//      //this.mapTransactions();
+//  }
+
+//  mapTransactions() {
+//   this.state.dates = this.state.trans.map(function(item) {
+//     return item['date'];
+//   });
+
+//   this.state.descriptions = this.state.trans.map(function(item) {
+//     return item['description'];
+//   });
+
+//   this.state.amounts = this.state.trans.map(function(item) {
+//     return item['amount'];
+//   });
+//   console.log(this.state.amounts);
+//   console.log(this.state.dates);
+//   console.log(this.state.descriptions)
+//   //console.log(this.state.trans)
+//   //console.log(this.state.transaction);
+// }
  
   render() {
-    return (
-      <View>
-        <Text> Transactions: {this.state.trans}</Text>
-      </View>
-    );
+    if(this.state.trans == []) {
+      return (
+        <View>
+          <Text> Transactions: </Text>
+          <Text> Date: {JSON.stringify(this.state.dates)}</Text> 
+          <Text> Descriptions: {JSON.stringify(this.state.descriptions)}</Text> 
+          <Text> Amount: {JSON.stringify(this.state.amounts)}</Text> 
+        </View>
+      );
+    }
+    if(this.state.trans != []) {
+      return (
+        <View>
+          <Text> Transactions: </Text>
+          <Text> Date: {JSON.stringify(this.state.dates)}</Text> 
+          <Text> Descriptions: {JSON.stringify(this.state.descriptions)}</Text> 
+          <Text> Amount: {JSON.stringify(this.state.amounts)}</Text> 
+        </View>
+      );
+    }
   }
 }
 
