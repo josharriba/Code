3f42cc068bd5f847804eb5bed5663a035c117060
Thel <thelg4@hotmail.com>
2022-04-25 23:14:26 -0400

    added tests for the finances screen

--- __tests__/UnitTests.js
+++ __tests__/UnitTests.js
@@ -1,5 +1,6 @@
 import SignupScreen from '../screens/SignupScreen';
 import LoginScreen from '../screens/LoginScreen';
+import FinancesScreen from '../screens/FinancesScreen';
 
 describe(SignupScreen.registerNewUser, () => {
     test("age should be 22", () => {
@@ -21,3 +22,26 @@ describe(LoginScreen.login, () => {
     });
     expect(LoginScreen.login(username, password)).toEqual(output)
 });
+
+// test for blanks
+describe(FinancesScreen.enterTransaction, () => {
+    test('transaction should not be left blank', () => {
+        const input = [
+            {date: '', description: '', amount: ''}
+        ];
+        const output = ['You cannot leave any information blank!']
+    });
+    expect(FinancesScreen.enterTransaction(date, description, amount)).toEqual(output)
+});
+
+// test for making sure an incorrect date format can't be entered
+describe(FinancesScreen.enterTransaction, () => {
+    test('transaction should not be left blank', () => {
+        const input = [
+            {date: '13/1/132', description: 'grocery', amount: '12'}
+        ];
+        const output = ['Please enter a valid date! (mm/dd/yy)']
+    });
+    expect(FinancesScreen.enterTransaction(date, description, amount)).toEqual(output)
+});
+
